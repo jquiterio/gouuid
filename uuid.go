837048fc59d071c92bd897b69d9a668e73a44ac4
Jorge Quiterio <jquiterio00@gmail.com>
2021-03-17 14:35:26 +0100

    first commit

---
+++ uuid.go
@@ -0,0 +1,79 @@
+package uuid
+
+import (
+	"bytes"
+	"crypto/rand"
+	"database/sql/driver"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type UUID [16]byte
+
+func randomize() (b []byte, err error) {
+	b = make([]byte, 16)
+	_, err = rand.Read(b)
+
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+func Compare(ua, ub UUID) bool {
+	return bytes.Equal(ua[:], ub[:])
+}
+
+func (uuid *UUID) String() string {
+	b := uuid
+	str := fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+	return strings.ToLower(str)
+}
+
+func ToUUID(str string) (uuid UUID, err error) {
+	if len(str) != 36 || str[8:9] != "-" || str[13:14] != "-" || str[18:19] != "-" || str[23:24] != "-" {
+		return uuid, errors.New("Invalid uuid")
+	}
+	b, err := hex.DecodeString(str[0:8] + str[9:13] + str[14:18] + str[19:23] + str[24:])
+	if err != nil {
+		return uuid, errors.New("Error decoding uuid")
+	}
+	_, err = io.ReadFull(bytes.NewBuffer(b), uuid[:])
+	return uuid, err
+}
+
+func New() UUID {
+	var uuid UUID
+	b, _ := randomize()
+	_, _ = io.ReadFull(bytes.NewBuffer(b), uuid[:])
+
+	uuid[6] = (uuid[6] & 0x0f) | 0x40 // Version 4
+	uuid[8] = (uuid[8] & 0x3f) | 0x80 // Variant is 10
+	return uuid
+}
+
+func (uuid UUID) Value() (driver.Value, error) {
+	str := uuid.String()
+	return str, nil
+}
+
+func (uuid *UUID) Scan(src interface{}) error {
+	switch src := src.(type) {
+	case nil:
+		return nil
+	case []byte:
+		if len(src) == 16 {
+			return uuid.Scan(string(src))
+		}
+		copy((*uuid)[:], src)
+		return nil
+	case string:
+		str := uuid.String()
+		_, err := ToUUID(str)
+		return err
+	default:
+		return fmt.Errorf("Unable to convert to UUID")
+	}
+}
---
+++ uuid_test.go
@@ -0,0 +1,36 @@
+package uuid
+
+import (
+	"fmt"
+	"testing"
+
+	"gopkg.in/check.v1"
+)
+
+func Test(t *testing.T) { check.TestingT(t) }
+
+type test struct{}
+
+var _ = check.Suite(&test{})
+
+func (s *test) TestNew(c *check.C) {
+	uuid := New()
+	str := uuid.String()
+	u, err := ToUUID(str)
+	//fmt.Println(str)
+
+	// from string to uuid
+
+	fmt.Println(u)
+	c.Assert(err, check.IsNil)
+	c.Assert(len(str), check.Equals, 36)
+	c.Assert(len(u), check.Equals, 16)
+}
+
+func BenchmarkUUID(b *testing.B) {
+	uuid := New()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		uuid.String()
+	}
+}
